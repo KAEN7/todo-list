7f069f66090d435fd4d9072e4261b30c2b0a4131
kaen <kusdsuna@naver.com>
2021-03-01 16:17:19 +0900

    Add style hiding scroll bar

--- src/components/TodoTable.js
+++ src/components/TodoTable.js
@@ -3,12 +3,23 @@ import styled from "styled-components";
 
 const TableBlock = styled.div`
   width: 700px;
+  max-height: 800px;
   height: 800px;
+
+  overflow-y: auto;
+  & {
+    -ms-overflow-style: none;
+    scrollbar-width: none;
+  }
+  &::-webkit-scrollbar {
+    display: none;
+  }
 
   display: flex;
   flex-direction: column;
   background: white;
   border-radius: 15px;
+  box-shadow: 0 0 8px 0 rgba(0, 0, 0, 0.04);
 
   margin: 0 auto;
   margin-top: 70px;
