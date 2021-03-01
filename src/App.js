628cad0dc8d14fb04dd9f0b1a66f4b0159804770
kaen <kusdsuna@naver.com>
2021-03-01 15:44:25 +0900

    Add GlobalStyle and TodoTable components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React from "react";
+// import { createStore } from "redux";
+import styled, { createGlobalStyle } from "styled-components";
+import TodoTable from "./components/TodoTable";
 
+// 스토어 생성
+// const store = createStore(rootReducer);
 
+// 글로벌 스타일
+const GlobalStyle = createGlobalStyle`
+  body {
+    background: #ced4da;
+  }
+`;
 
 function App() {
   return (
-   
+    // <Provider store={store}>
+    <>
+      <GlobalStyle />
+      <TodoTable />
+    </>
+    // </Provider>
   );
 }
 
---
+++ src/components/TodoTable.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styled from "styled-components";
+
+function TodoTable() {
+  return <div>hello</div>;
+}
+
+export default TodoTable;
