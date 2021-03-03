10469f090b84d4824de9b7df33ce2bcb59266c18
kaen <kusdsuna@naver.com>
2021-03-03 20:09:33 +0900

    Add button styled

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -2,21 +2,47 @@ import React from "react";
 import styled from "styled-components";
 import { FiPlus } from "react-icons/fi";
 
+// styled
 const FormBlock = styled.div`
   background: gray;
   display: flex;
   justify-content: center;
 `;
 
+const Form = styled.form`
+  display: flex;
+  height: 30px;
+`;
+
+const CreateInput = styled.input`
+  width: 400px;
+  border: none;
+  outline: none;
+`;
+
+const CreateBtn = styled.button`
+  background: white;
+  border: none;
+  outline: none;
+  &:active {
+    transform: 0.7;
+  }
+`;
+
+// function
+const clickBtn = (e) => {
+  e.preventDefault();
+};
+
 function TodoForm() {
   return (
     <FormBlock>
-      <form>
-        <input autoFocus placeholder="할 일을 적어보세요" />
-        <button>
+      <Form>
+        <CreateInput autoFocus placeholder="할 일을 적어보세요" />
+        <CreateBtn onClick={clickBtn}>
           <FiPlus />
-        </button>
-      </form>
+        </CreateBtn>
+      </Form>
     </FormBlock>
   );
 }
