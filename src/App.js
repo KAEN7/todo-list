e32c7df8d63c05069635d5d3e5738163d0cfefda
kaen <kusdsuna@naver.com>
2021-03-13 21:13:42 +0900

    클론코딩할거 함수형으로 변환

--- src/App.js
+++ src/App.js
@@ -1,157 +1,168 @@
-import React from 'react';
-import TodoList from './TodoList';
-import Nav from './Nav';
-import SearchList from './SearchList';
+import React, { useState } from "react";
+import TodoList from "./components/TodoList";
+import Nav from "./components/Nav";
+import SearchList from "./components/SearchList";
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      todoList: [],
-      currentCategoryTodoList: [],
-      currentCategory: { id: 1, name: '미분류' },
-      completedTodoList: [],
-      searchResult: [],
-      isSearching: false,
-    };
+function App() {
+  const [state, setState] = useState({
+    todoList: [], // 투두리스트
+    currentCategoryTodoList: [], // 현재 투두리스트 카테고리
+    currentCategory: { id: 1, name: "미분류" }, // 현재 카테고리
+    completedTodoList: [], // 완료한 투두리스트
+    searchResult: [], // 검색 결과
+    isSearching: false, // 검색 상태
+    id: 0, // 아이디
+  });
 
-    this.viewCurrentCategoryTodoList = this.viewCurrentCategoryTodoList.bind(this);
-    this.updateTodoList = this.updateTodoList.bind(this);
-    this.removeTodo = this.removeTodo.bind(this);
-    this.removeCategory = this.removeCategory.bind(this);
-    this.toggleTodoComplete = this.toggleTodoComplete.bind(this);
-    this.updateTodoText = this.updateTodoText.bind(this);
-    this.updateCategoryName = this.updateCategoryName.bind(this);
-    this.searchTodo = this.searchTodo.bind(this);
-    this.setSearchingToFalse = this.setSearchingToFalse.bind(this);
-  }
+  // 구조분해
+  const {
+    todoList,
+    currentCategoryTodoList,
+    currentCategory,
+    completedTodoList,
+    searchResult,
+    isSearching,
+    id,
+  } = state;
 
-  id = 0;
-
-  viewCurrentCategoryTodoList(category, todoArr = this.state.todoList, searchResult = []) {
-    this.setState({
+  // 현재 투두리스트 카테고리 보는 함수
+  function viewCurrentCategoryTodoList({
+    category, // 카테고리
+    todoArr = todoList, // todoArr에 투두리스트를 담아줌
+    searchResult = [], // 왜 빈값을 다시 담아준건지 모르겠음
+  }) {
+    // 상태 변경
+    setState({
       todoList: todoArr,
       currentCategory: category,
       currentCategoryTodoList: todoArr.filter(
-        todo => todo.category.name === category.name && todo.isComplete === false,
+        (todo) =>
+          todo.category.name === category.name && todo.isComplete === false
       ),
       completedTodoList: todoArr.filter(
-        todo => todo.category.name === category.name && todo.isComplete === true,
+        (todo) =>
+          todo.category.name === category.name && todo.isComplete === true
       ),
       searchResult: searchResult,
     });
   }
 
-  updateTodoList(text) {
+  function updateTodoList(text) {
     const newTodo = {
-      id: this.id,
+      id: nextId,
       text: text,
-      category: this.state.currentCategory,
+      category: currentCategory,
       isComplete: false,
     };
-    const updatedArr = this.state.todoList.concat(newTodo);
-    this.id = this.id + 1;
-    this.viewCurrentCategoryTodoList(this.state.currentCategory, updatedArr);
+    const updatedArr = todoList.concat(newTodo);
+    const nextId = id + 1;
+    viewCurrentCategoryTodoList(currentCategory, updatedArr);
   }
 
-  removeTodo(id) {
-    const removedArr = this.state.todoList.filter(todo => todo.id !== id);
-    this.viewCurrentCategoryTodoList(this.state.currentCategory, removedArr);
+  function removeTodo(id) {
+    const removedArr = todoList.filter((todo) => todo.id !== id);
+    viewCurrentCategoryTodoList(currentCategory, removedArr);
   }
 
-  removeCategory(category) {
-    const removedArr = this.state.todoList.filter(todo => todo.category.name !== category.name);
-    this.viewCurrentCategoryTodoList({ id: 0, name: '미분류' }, removedArr);
+  function removeCategory(category) {
+    const removedArr = todoList.filter(
+      (todo) => todo.category.name !== category.name
+    );
+    viewCurrentCategoryTodoList({ id: 0, name: "미분류" }, removedArr);
   }
 
-  toggleTodoComplete(id) {
-    const completedTodo = this.state.todoList.filter(todo => todo.id === id)[0];
+  function toggleTodoComplete(id) {
+    const completedTodo = todoList.filter((todo) => todo.id === id)[0];
     completedTodo.isComplete = !completedTodo.isComplete;
-    const changedArr = this.state.todoList.filter(todo => todo.id !== id).concat(completedTodo);
+    const changedArr = todoList
+      .filter((todo) => todo.id !== id)
+      .concat(completedTodo);
     changedArr.sort((a, b) => a.id - b.id);
 
-    this.viewCurrentCategoryTodoList(this.state.currentCategory, changedArr);
+    viewCurrentCategoryTodoList(currentCategory, changedArr);
   }
 
-  updateTodoText(id, text) {
-    const updatedTodo = this.state.todoList.filter(todo => todo.id === id)[0];
+  function updateTodoText(id, text) {
+    const updatedTodo = this.todoList.filter((todo) => todo.id === id)[0];
     updatedTodo.text = text;
-    const changedArr = this.state.todoList.filter(todo => todo.id !== id).concat(updatedTodo);
+    const changedArr = todoList
+      .filter((todo) => todo.id !== id)
+      .concat(updatedTodo);
     changedArr.sort((a, b) => a.id - b.id);
 
-    this.viewCurrentCategoryTodoList(this.state.currentCategory, changedArr);
+    viewCurrentCategoryTodoList(currentCategory, changedArr);
   }
 
-  updateCategoryName(targetCategory) {
-    const updatedTodo = this.state.todoList.filter(todo => todo.category.id === targetCategory.id);
-    updatedTodo.forEach(todo => (todo.category.name = targetCategory.name));
-    const changedArr = this.state.todoList
-      .filter(todo => todo.category.id !== targetCategory.id)
+  function updateCategoryName(targetCategory) {
+    const updatedTodo = todoList.filter(
+      (todo) => todo.category.id === targetCategory.id
+    );
+    updatedTodo.forEach((todo) => (todo.category.name = targetCategory.name));
+    const changedArr = todoList
+      .filter((todo) => todo.category.id !== targetCategory.id)
       .concat(updatedTodo);
 
     changedArr.sort((a, b) => a.id - b.id);
 
-    this.viewCurrentCategoryTodoList(targetCategory, changedArr);
+    viewCurrentCategoryTodoList(targetCategory, changedArr);
   }
 
-  searchTodo(word) {
+  function searchTodo(word) {
     if (word.length > 0) {
-      this.setState({
+      setState({
         isSearching: true,
       });
       const searchTitle = {
         id: 0,
         name: `'${word}'에 대한 검색결과`,
       };
-      const regex = new RegExp(word, 'gi');
-      const targetTodosArr = this.state.todoList.filter(todo => todo.text.match(regex));
-      this.viewCurrentCategoryTodoList(searchTitle, undefined, targetTodosArr);
+      const regex = new RegExp(word, "gi");
+      const targetTodosArr = todoList.filter((todo) => todo.text.match(regex));
+      viewCurrentCategoryTodoList(searchTitle, undefined, targetTodosArr);
     } else {
-      this.setState({
+      setState({
         isSearching: false,
       });
-      this.viewCurrentCategoryTodoList({ id: 1, name: '미분류' });
+      viewCurrentCategoryTodoList({ id: 1, name: "미분류" });
     }
   }
 
-  setSearchingToFalse() {
-    this.setState({
+  function setSearchingToFalse() {
+    setState({
       isSearching: false,
     });
   }
 
-  render() {
-    return (
-      <main>
-        <Nav
-          viewCurrentCategoryTodoList={this.viewCurrentCategoryTodoList}
-          removeCategory={this.removeCategory}
-          updateCategoryName={this.updateCategoryName}
-          searchTodo={this.searchTodo}
-          setSearchingToFalse={this.setSearchingToFalse}
+  return (
+    <main>
+      <Nav
+        viewCurrentCategoryTodoList={viewCurrentCategoryTodoList}
+        removeCategory={removeCategory}
+        updateCategoryName={updateCategoryName}
+        searchTodo={searchTodo}
+        setSearchingToFalse={setSearchingToFalse}
+      />
+      {isSearching ? (
+        <SearchList
+          currentCategory={currentCategory}
+          searchResult={searchResult}
+          viewCurrentCategoryTodoList={viewCurrentCategoryTodoList}
+          setSearchingToFalse={setSearchingToFalse}
         />
-        {this.state.isSearching ? (
-          <SearchList
-            currentCategory={this.state.currentCategory}
-            searchResult={this.state.searchResult}
-            viewCurrentCategoryTodoList={this.viewCurrentCategoryTodoList}
-            setSearchingToFalse={this.setSearchingToFalse}
-          />
-        ) : (
-          <TodoList
-            currentCategory={this.state.currentCategory}
-            updateTodoList={this.updateTodoList}
-            currentCategoryTodoList={this.state.currentCategoryTodoList}
-            removeTodo={this.removeTodo}
-            removeCategory={this.removeCategory}
-            completedTodoList={this.state.completedTodoList}
-            toggleTodoComplete={this.toggleTodoComplete}
-            updateTodoText={this.updateTodoText}
-          />
-        )}
-      </main>
-    );
-  }
+      ) : (
+        <TodoList
+          currentCategory={currentCategory}
+          updateTodoList={updateTodoList}
+          currentCategoryTodoList={currentCategoryTodoList}
+          removeTodo={removeTodo}
+          removeCategory={removeCategory}
+          completedTodoList={completedTodoList}
+          toggleTodoComplete={toggleTodoComplete}
+          updateTodoText={updateTodoText}
+        />
+      )}
+    </main>
+  );
 }
 
 export default App;
