32779e4c8efbfcc9fe362942e17018b4e3fc0724
kaen <kusdsuna@naver.com>
2021-03-14 01:29:53 +0900

    Header 스타일 및 주석추가

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,25 @@
 import React from "react";
+import styled from "styled-components";
+
+const HeaderBlock = styled.h2`
+  margin: 0 auto;
+  padding: 10px;
+  font-size: 30px;
+`;
 
 function Header() {
-  let today = new Date();
+  let today = new Date(); // 날짜
 
-  let year = today.getFullYear();
-  let month = today.getMonth() + 1;
-  let date = today.getDate();
-  let dayList = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
-  let day = dayList[today.getDay()];
+  let year = today.getFullYear(); // 연도
+  let month = today.getMonth() + 1; // 월
+  let date = today.getDate(); // 일
+  let dayList = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"]; // 요일리스트
+  let day = dayList[today.getDay()]; // 요일 숫자로 불러와짐
 
   return (
-    <h2>
+    <HeaderBlock>
       {year}. {month}. {date}. {day}
-    </h2>
+    </HeaderBlock>
   );
 }
 
