56be59660500c8ae5d3f7c41094f62aff040ce54
kaen <kusdsuna@naver.com>
2021-03-14 22:56:32 +0900

    투두아이템 추가 기능 생성

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import Header from "./components/Header";
 import List from "./components/List";
 
 // styled-components
+
+// 글로벌 스타일
 const GlobalStyle = createGlobalStyle`
   #root {
     display: flex;
@@ -18,8 +20,14 @@ const GlobalStyle = createGlobalStyle`
     justify-content: center;
     margin-top: 88px;
   }
+
+  input {
+    outline: none;
+    border: none;
+  }
 `;
 
+// 네비게이션 스타일
 const Nav = styled.div`
   background: white;
   display: flex;
@@ -28,8 +36,11 @@ const Nav = styled.div`
   box-shadow: 0.7px 0.7px #ced4da;
   width: 200px;
   height: 400px;
+  box-sizing: border-box;
+  padding: 10px;
 `;
 
+// 메인 스타일
 const Main = styled.div`
   background: white;
   display: flex;
@@ -38,6 +49,8 @@ const Main = styled.div`
   box-shadow: 0.7px 0.7px #ced4da;
   width: 800px;
   height: 700px;
+  box-sizing: border-box;
+  padding: 10px;
 `;
 
 function App() {
--- src/components/List.js
+++ src/components/List.js
@@ -1,16 +1,89 @@
-import React from "react";
+import React, { useState } from "react";
+import styled from "styled-components";
+
+const ListBlock = styled.div`
+  display: flex;
+  flex-direction: column;
+  box-sizing: border-box;
+  padding: 10px;
+  input {
+    width: 100%;
+    font-size: 19px;
+    margin-top: 30px;
+    &::placeholder {
+      color: #adb5bd;
+      text-align: center;
+    }
+  }
+`;
+
+const TodolistBlock = styled.div`
+  margin: 2;
+  padding: 0;
+`;
+
+const ListItemBlock = styled.div`
+  display: flex;
+  justify-content: space-between;
+  div {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    height: 30px;
+    margin-top: 5px;
+  }
+`;
+
+function ListItem({ key, id, text, done }) {
+  return (
+    <ListItemBlock>
+      <div>O</div>
+      <div done={done}>{text}</div>
+      <div>삭제</div>
+    </ListItemBlock>
+  );
+}
 
 function List() {
+  const [todos, setTodos] = useState([
+    {
+      key: 0,
+      id: 0,
+      text: "",
+      done: false,
+    },
+  ]);
+
+  // 구조분해
+  const { key, id, text, done } = todos;
+
+  // FUNCTION
+  const onChange = (e) => {
+    const newTodo = {
+      key: key + 1,
+      id: id + 1,
+      text: e.target.value,
+      done: false,
+    };
+    const newArray = todos.concat(newTodo);
+    setTodos = newArray;
+  };
+
   return (
-    <>
+    <ListBlock>
       <div>완료한 일 3개</div>
-      <ul>
-        <li>1</li>
-        <li>2</li>
-        <li>3</li>
-      </ul>
-      <input placeholder="할 일을 입력해주세요" />
-    </>
+      <TodolistBlock>
+        {todos.map((todo) => (
+          <ListItem
+            key={todo.id}
+            id={todo.id}
+            text={todo.text}
+            done={todo.done}
+          />
+        ))}
+      </TodolistBlock>
+      <input placeholder="할 일을 입력해주세요" onChange={onChange} />
+    </ListBlock>
   );
 }
 
