e7f315eef92d6ecb48b8692a23362655690c26b3
kaen <kusdsuna@naver.com>
2021-07-15 22:07:55 +0900

    Login 컴포넌트 틀잡기

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,14 @@
 import React from "react";
+// 서버연결
 
 function Login() {
-  return <div>Login</div>;
+  return (
+    <>
+      <div>Login</div>
+      <input placeholder="아이디를 입력해주세요" />
+      <input type="password" placeholder="비밀번호를 입력해주세요" />
+    </>
+  );
 }
 
 export default Login;
