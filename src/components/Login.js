72afaad683986a94818737d711650c63d77146c9
kaen <kusdsuna@naver.com>
2021-07-16 22:57:09 +0900

    Login 컴포넌트 스타일 초기 세팅

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,13 +1,52 @@
 import React from "react";
+import styled from "styled-components";
 // 서버연결
+
+const BckLogin = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+const LoginBlock = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 30px;
+  width: 300px;
+  height: 300px;
+  border: 1px solid;
+  margin-top: 230px;
+  .header {
+    font-size: 45px;
+    margin-bottom: 55px;
+  }
+  .id {
+    outline: none;
+    border: none;
+    // 밑줄넣기
+    font-size: 20px;
+    margin-bottom: 30px;
+  }
+  .pwd {
+    outline: none;
+    border: none;
+    font-size: 20px;
+  }
+`;
 
 function Login() {
   return (
-    <>
-      <div>Login</div>
-      <input placeholder="아이디를 입력해주세요" />
-      <input type="password" placeholder="비밀번호를 입력해주세요" />
-    </>
+    <BckLogin>
+      <LoginBlock>
+        <div className="header">LOGIN</div>
+        <input className="id" placeholder="아이디를 입력해주세요" />
+        <input
+          className="pwd"
+          type="password"
+          placeholder="비밀번호를 입력해주세요"
+        />
+      </LoginBlock>
+    </BckLogin>
   );
 }
 
